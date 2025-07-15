14b3d6c8637e31e49962560e515a64362b1da0d5
Lucie Aloccio <114408320+luciealc@users.noreply.github.com>
2025-07-15 15:56:01 +0200

    feat/docs: swagger ui documentation for authentication api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 
 const app = express();
 
@@ -11,8 +12,9 @@ app.use(cors({
 app.use(express.json());
 
 const userRoute = require('./routes/user.js');
-
 app.use('', userRoute);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
